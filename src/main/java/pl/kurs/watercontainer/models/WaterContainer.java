299ae436b82d389c8d066a3090909c4ae9c4474f
KAROL SOKOLOWSKI <k.sokolowski11@gmail.com>
2024-03-15 14:06:56 +0100

    added WaterContainer model

---
+++ src/main/java/pl/kurs/watercontainer/models/WaterContainer.java
@@ -0,0 +1,40 @@
+package pl.kurs.watercontainer.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class WaterContainer implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private double maxCapacity;
+    private double waterLevel;
+
+    public WaterContainer(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WaterContainer that = (WaterContainer) o;
+        return Double.compare(maxCapacity, that.maxCapacity) == 0 && Double.compare(waterLevel, that.waterLevel) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, maxCapacity, waterLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
+}
