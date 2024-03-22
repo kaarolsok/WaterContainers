a0a644f9ffbfb4a6681fb9cf275673eb8e7636c1
KAROL SOKOLOWSKI <k.sokolowski11@gmail.com>
2024-03-22 12:01:09 +0100

    added complete logic in service

--- src/main/java/pl/kurs/watercontainer/services/WaterContainerService.java
+++ src/main/java/pl/kurs/watercontainer/services/WaterContainerService.java
@@ -5,25 +5,65 @@ import pl.kurs.watercontainer.models.WaterContainer;
 public class WaterContainerService {
 
     public WaterContainer findContainerWithMostWater(WaterContainer[] containers){
-        if (containers.length == 0){
-            return null;
+        int index = findFirstNonNullContainerIndex(containers);
+        WaterContainer containerWithMostWater = containers[index];
+
+
+        for (int k = index; k < containers.length - 1; k++) {
+            if (containers[k + 1] != null && containers[k + 1].getWaterLevel() > containerWithMostWater.getWaterLevel()){
+                containerWithMostWater = containers[k + 1];
+            }
         }
-        WaterContainer containerWithMostWater = null;
+        return containerWithMostWater;
+    }
+
+    public double getPercentageFulfillment(WaterContainer container){
+        return container.getWaterLevel() / container.getMaxCapacity();
+    }
+
+    public WaterContainer findMostFiledContainer(WaterContainer[] containers){
+        int index = findFirstNonNullContainerIndex(containers);
+        WaterContainer mostFiledContainer = containers[index];
+
+        for (int i = index; i < containers.length - 1; i++) {
+            if (containers[i + 1] != null){
+                if (getPercentageFulfillment(containers[i + 1]) > getPercentageFulfillment(mostFiledContainer))
+                    mostFiledContainer = containers[i + 1];
+            }
+        }
+        return mostFiledContainer;
+    }
+
+    public WaterContainer[] findEmptyContainers(WaterContainer[] containers){
+        int emptyContainersCounter = 0;
+        for (WaterContainer container : containers) {
+            if (container != null && container.getWaterLevel() == 0){
+                emptyContainersCounter++;
+            }
+        }
+        WaterContainer[] emptyContainersArray = new WaterContainer[emptyContainersCounter];
+        int index = 0;
+
+        for (int i = 0; i < containers.length; i++) {
+            if (containers[i] != null && containers[i].getWaterLevel() == 0){
+                emptyContainersArray[index++] = containers[i];
+            }
+        }
+        return emptyContainersArray;
+    }
+
+    private int findFirstNonNullContainerIndex(WaterContainer[] containers){
+        if (containers.length == 0){
+            throw new RuntimeException("Array is empty");
+        }
+
         int index = 0;
         for (int i = 0; i < containers.length; i++) {
             if (containers[i] != null){
-                containerWithMostWater = containers[i];
                 index = i;
                 break;
             }
         }
-
-
-        for (int k = index; k < containers.length; k++) {
-            if (containers[k] != null && containers[k].getWaterLevel() > containerWithMostWater.getWaterLevel()){
-                containerWithMostWater = containers[k];
-            }
-        }
-        return containerWithMostWater;
+        return index;
     }
 }
