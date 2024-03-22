6589b372de7d3cbc49b629c415ccede6168a1254
KAROL SOKOLOWSKI <k.sokolowski11@gmail.com>
2024-03-22 11:14:11 +0100

    deleted commented code

--- src/main/java/pl/kurs/watercontainer/services/WaterContainerService.java
+++ src/main/java/pl/kurs/watercontainer/services/WaterContainerService.java
@@ -18,14 +18,6 @@ public class WaterContainerService {
             }
         }
 
-        /*
-        wskazywało niewłaściwy pojemnik
-        for (int k = index; k < containers.length - 1; k++){
-//            if (containers[k] != null && containers[k + 1] != null && (containers[k].getWaterLevel() < containers[k + 1].getWaterLevel())) {
-//                containerWithMostWater = containers[k + 1];
-//            }
-//        }
-         */
 
         for (int k = index; k < containers.length; k++) {
             if (containers[k] != null && containers[k].getWaterLevel() > containerWithMostWater.getWaterLevel()){
